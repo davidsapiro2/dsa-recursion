1229974e1facf237867c6ba4f381003954878115
David Sapiro <dsap48@gmail.com>
2024-04-23 12:07:35 -0700

    solved product, every other, longest word, find

--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -3,7 +3,9 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-  return 42;
+  if (words === null) return 0;
+  const longestWord = longest(words.next)
+  return words.val.length > longestWord ? words.val.length : longestWord;
 }
 
 export { longest };
